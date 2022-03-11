dc4e57fcd8442d12a3853c4c60e39bb90ca2d958
Wynnnnn <sh2561@york.ac.uk>
2022-03-11 12:19:17 +0000

    Whirlpool animation, map and background graphics

--- core/assets/islands.tsx
+++ core/assets/islands.tsx
@@ -33,12 +33,25 @@
  </tile>
  <tile id="73">
   <image width="64" height="64" source="tile_01.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0.625" y="53.75" height="0.5">
+    <ellipse/>
+   </object>
+   <object id="5" x="1.25" y="63.875">
+    <polygon points="0,0 0.875,-27.25 5.375,-39.125 14.875,-49 31.625,-55.125 49,-60 62.875,-62.75 62.625,0.125"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="74">
   <image width="64" height="64" source="tile_02.png"/>
  </tile>
  <tile id="75">
   <image width="64" height="64" source="tile_03.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="1">
+    <polygon points="0,0 23.5,0.375 40.25,4.875 48,14.25 55.875,33.75 63.125,57 63.5,63.125 0,62.875"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="76">
   <image width="64" height="64" source="tile_04.png"/>
@@ -48,6 +61,11 @@
  </tile>
  <tile id="78">
   <image width="64" height="64" source="tile_06.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.75" y="64.25">
+    <polygon points="0,0 0.75,-26.75 9,-43.875 22.375,-52.375 33.625,-55.875 50.375,-61.25 63.125,-63.5 63.625,-0.75 -1.75,0.375"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="79">
   <image width="64" height="64" source="tile_07.png"/>
@@ -57,15 +75,30 @@
  </tile>
  <tile id="81">
   <image width="64" height="64" source="tile_09.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="0.625">
+    <polygon points="0,0 24,0.5 36,4.125 48.875,13.375 53.25,28.375 63.75,63 -0.5,63.125"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="82">
   <image width="64" height="64" source="tile_33.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.5" y="0.25">
+    <polygon points="0,0 11,36.75 17,49.625 23.125,56.25 36.375,62.25 63,63 63,-0.375"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="83">
   <image width="64" height="64" source="tile_34.png"/>
  </tile>
  <tile id="84">
   <image width="64" height="64" source="tile_35.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.125" y="62.625">
+    <polygon points="0,0 28.5,-5.75 53,-16 62.75,-34.75 63.5,-47.25 63.875,-63 -0.5,-62.75"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="85">
   <image width="64" height="64" source="tile_36.png"/>
@@ -93,6 +126,11 @@
  </tile>
  <tile id="93">
   <image width="64" height="64" source="tile_54.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.125" y="0">
+    <polygon points="0,0 7.5,32.625 16.5,52.25 29.75,60.625 48.375,64.25 63.375,63.5 63.75,-0.125"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="94">
   <image width="64" height="64" source="tile_55.png"/>
@@ -102,6 +140,11 @@
  </tile>
  <tile id="96">
   <image width="64" height="64" source="tile_57.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="63.875">
+    <polygon points="0,0 33.5,-8.5 54.875,-19.875 64.625,-42.5 64.125,-64.125 -0.75,-64.625"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="100">
   <image width="64" height="64" source="tile_04.png"/>
@@ -111,12 +154,22 @@
  </tile>
  <tile id="106">
   <image width="64" height="64" source="tile_10.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.625" y="63.625">
+    <polygon points="0,0 3.375,-32 11.75,-44.625 37,-54.75 62.125,-61 62.125,0.5"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="107">
   <image width="64" height="64" source="tile_11.png"/>
  </tile>
  <tile id="108">
   <image width="64" height="64" source="tile_12.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="2.375">
+    <polygon points="0,0 25.375,1.625 44.25,10.75 60.75,52.25 60.875,61.625 -1.75,62.5"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="109">
   <image width="64" height="64" source="tile_13.png"/>
@@ -165,6 +218,11 @@
  </tile>
  <tile id="129">
   <image width="64" height="64" source="tile_33.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.25" y="0.375">
+    <polygon points="0,0 10.125,36.5 19,53.75 40.375,63.375 63.875,62.75 64.5,-1.125"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="130">
   <image width="64" height="64" source="tile_34.png"/>
@@ -192,12 +250,22 @@
  </tile>
  <tile id="138">
   <image width="64" height="64" source="tile_42.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.875" y="0.375">
+    <polygon points="0,0 9.75,33.875 17.75,49.375 24.5,55.625 39.625,60.875 64.5,61.375 62.875,-1.75"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="139">
   <image width="64" height="64" source="tile_43.png"/>
  </tile>
  <tile id="140">
   <image width="64" height="64" source="tile_44.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.125" y="62.5">
+    <polygon points="0,0 42.875,-14.5 54.75,-21.125 62.75,-43.125 62.125,-63.875 -2.5,-63"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="141">
   <image width="64" height="64" source="tile_45.png"/>
@@ -212,13 +280,28 @@
   <image width="64" height="64" source="tile_48.png"/>
  </tile>
  <tile id="145">
-  <image width="64" height="64" source="tile_49.png"/>
+  <image width="34" height="34" source="tile_49.png"/>
+  <objectgroup draworder="index" id="4">
+   <object id="5" x="8.4375" y="0.375">
+    <polygon points="0,0 -7.9375,15.4375 -8.125,27.875 11.3125,33.5 25.125,33.625 25.625,16.1875 13.375,-0.1875"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="146">
-  <image width="64" height="64" source="tile_50.png"/>
+  <image width="54" height="46" source="tile_50.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="17.1818" y="0.636364">
+    <polygon points="0,0 -16.9091,8.27273 -10.2727,20.1818 -13.6364,31.9091 -2.45455,39.6364 19,45.3636 21.3636,45.5455 36.1818,34.4545 37.4545,18.1818 29.1818,7 15.3636,6.63636 0.636364,-0.818182"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="147">
-  <image width="64" height="64" source="tile_51.png"/>
+  <image width="40" height="40" source="tile_51.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.272727" y="13.3636">
+    <polygon points="0,0 0.818182,19.5455 27.2727,27.7273 41.4545,5.54545 24.9091,-13.8182 -0.181818,-1.54545"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="148">
   <image width="64" height="64" source="tile_52.png"/>
@@ -227,13 +310,28 @@
   <image width="64" height="64" source="tile_53.png"/>
  </tile>
  <tile id="161">
-  <image width="64" height="64" source="tile_65.png"/>
+  <image width="34" height="34" source="tile_65.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.363636" y="14.3636">
+    <polygon points="0,0 8.90909,-14.5455 22.9091,-14.2727 34,1.09091 33.9091,19 30.4545,20.8182 0.181818,16.1818"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="162">
-  <image width="64" height="64" source="tile_66.png"/>
+  <image width="54" height="46" source="tile_66.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.63636" y="34.9091">
+    <polygon points="0,0 34.3636,11.6364 49.5455,-0.363636 50.7273,-16.5455 42.6364,-27.6364 30.4545,-28.0909 14.3636,-35.7273 -3.72727,-27.4545 -3.18182,-20.1818 2.18182,-13.7273"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="163">
-  <image width="64" height="64" source="tile_67.png"/>
+  <image width="40" height="40" source="tile_67.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.909091" y="34">
+    <polygon points="0,0 25.3636,6 39.9091,-14.8182 23.1818,-34.1818 -1,-21.9091"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="164">
   <image width="64" height="64" source="tile_68.png"/>
