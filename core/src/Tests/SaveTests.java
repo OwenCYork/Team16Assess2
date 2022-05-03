d005b7438a84843690eaaa5087ed4a4be56d7ba3
AlanYJZ <jy1627@york.ac.uk>
2022-05-03 02:13:12 +0100

    New Savetest

--- core/src/Tests/SaveTests.java
+++ core/src/Tests/SaveTests.java
@@ -14,33 +14,38 @@ import static org.mockito.Mockito.spy;
 
 
 public class SaveTests {
-    /*
+/*
     private String Health;
+    private String Score;
+    private String Coins;
+    private String HealthRe;
     private Boolean checker;
 
     @Test
-    public void SavefileExistance() throws IOException {
-        PirateGame p = spy(new PirateGame());
-        SaveScreen s = new SaveScreen(p,true);
+    public void SavefileHealth() throws IOException {
 
-        try {
-            s.createFile();
-            File file = new File("core\\assets\\saved.txt");
-            InputStreamReader inputReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
-            BufferedReader bf = new BufferedReader(inputReader);
-            Health = bf.readLine();
+        File file = new File("core\\assets\\saved.txt");
+        InputStreamReader inputReader = new InputStreamReader(new FileInputStream(file),"UTF-8");
+        BufferedReader bf = new BufferedReader(inputReader);
+        Health = bf.readLine();
 
-            checker = Health.isEmpty();
-            assertTrue("Save file exist", checker);
-        } catch(Exception e){
-            assertTrue(e.toString(), false);
-        }
+        checker = Health.isEmpty();
+        assertTrue("Health exist in savefile.", checker);
+
+    }
+    @Test
+    public void SavefileScore() throws IOException {
+
+
+        Score = bf.readLine();
+        checker = Score.isEmpty();
+        assertTrue("Score exist in save file.", checker);
+
     }
 
-     */
 
 
-
+*/
 }
     
 
